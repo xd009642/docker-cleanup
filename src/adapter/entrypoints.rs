a93615acfe3da4556a5eb05ef340a4b7892acaf1
xd009642 <danielmckenna93@gmail.com>
2025-07-29 15:22:50 +0100

    jsonlines support

--- src/adapter/entrypoints.rs
+++ src/adapter/entrypoints.rs
@@ -6,13 +6,28 @@ use trustfall::provider::{ResolveInfo, VertexIterator};
 use super::vertex::Vertex;
 
 pub(super) fn image<'a>(_resolve_info: &ResolveInfo) -> VertexIterator<'a, Vertex> {
+    let version = Command::new("docker")
+        .args(["--version"])
+        .output()
+        .expect("couldn't get docker version");
+    let version = String::from_utf8_lossy(&version.stdout);
+    let is_podman = version.contains("podman");
+
     let images = Command::new("docker")
         .args(["image", "ls", "--format", "json"])
         .output()
         .expect("failed to run docker");
 
-    let images: Vec<ImageOutput> =
-        serde_json::from_slice(&images.stdout).expect("couldn't deserialize the json output");
+    let images: Vec<ImageOutput> = if is_podman {
+        serde_json::from_slice(&images.stdout).expect("couldn't deserialize the json output")
+    } else {
+        let s = String::from_utf8_lossy(&images.stdout);
+        let mut v = vec![];
+        for line in s.lines() {
+            v.push(serde_json::from_str(line).expect("couldn't deserialize the json output"));
+        }
+        v
+    };
 
     Box::new(
         images
